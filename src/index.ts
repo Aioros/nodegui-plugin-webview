5d0d6eeb2d8ae714735305c1e42ff1ea3cbbaf48
Aioros <aioros83@gmail.com>
2024-03-20 10:02:58 -0400

    Updated package.json

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 //@ts-ignore
-import "../build/Release/nodegui_plugin_webview_init.node";
+//import "../build/Release/nodegui_plugin_webview_init.node";
 
 export { QWebEngineView, QWebEngineViewSignals } from "./lib/QWebEngineView";
 export { QWebChannel } from "./lib/QWebChannel";
